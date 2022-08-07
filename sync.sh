275c588db0a8430044408fa99de40f15d7fc17e0
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-08-07 18:04:21 +0800

    Exclude LICENSE in sync script

--- sync.sh
+++ sync.sh
@@ -1,8 +1,25 @@
 #!/bin/bash -e
 
-files=($(git ls-tree --full-tree --name-only -r HEAD | grep -v "^$(basename "$0")\$"))
+excluded() {
+    local excludes=(
+        "$(basename "$0")"
+        "LICENSE"
+    )
+    local file
+    for file in "${excludes[@]}"; do
+        if [ "$1" = "$file" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+files=($(git ls-tree --full-tree --name-only -r HEAD))
 
 for file in "${files[@]}"; do
+    if excluded "$file"; then
+        continue
+    fi
     cp "$HOME/$file" "$file"
     echo "$file"
 done
